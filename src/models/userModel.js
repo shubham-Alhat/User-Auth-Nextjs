61d1759f0b8f4915f0286fb6fa277bd3780dc446
shubham-Alhat <shubhamalhat520@gmail.com>
2025-06-26 21:10:54 +0530

    Add user model schema for authentication system

---
+++ src/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgetPasswordToken: String,
+    forgetPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
